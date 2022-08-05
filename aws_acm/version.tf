0f21826654e3d37b0f93068eb8aafe8b1b5592a0
Yevhenii Rosliakov <ei.roslyakov@gmail.com>
2022-08-05 14:45:40 +0300

    added versions

--- aws_acm/version.tf
+++ aws_acm/version.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 0.13.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = "~> 3"
     }
   }
+  required_version = ">= 0.14"
 }
--- aws_sns/version.tf
+++ aws_sns/version.tf
@@ -5,5 +5,5 @@ terraform {
       version = "~> 3"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.13"
 }
