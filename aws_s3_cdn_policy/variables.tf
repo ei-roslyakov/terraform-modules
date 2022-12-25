4d1739c00ec64252a64061829144933daa2ed683
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-12-25 17:16:52 +0200

    fmt

--- aws_s3_cdn_policy/variables.tf
+++ aws_s3_cdn_policy/variables.tf
@@ -1,10 +1,10 @@
 variable "bucket" {
-    description = "The name of the bucket to which to apply the policy."
-    default = ""
+  description = "The name of the bucket to which to apply the policy."
+  default     = ""
 }
 
 variable "iam_arn" {
-    description = "A pre-generated ARN for use in S3 bucket policies (see below). Example: arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2QWRUHAPOMQZL"
-    default = ""
+  description = "A pre-generated ARN for use in S3 bucket policies (see below). Example: arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2QWRUHAPOMQZL"
+  default     = ""
 }
 
