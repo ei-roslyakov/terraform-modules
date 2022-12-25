dd8ad729c2f20c449265860b12039acf434d1d82
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-12-25 17:15:46 +0200

    added s3_policy module

---
+++ aws_s3_cdn_policy/README.md
@@ -0,0 +1,8 @@
+## Usage
+
+```hcl
+inputs = {
+  bucket  = "bucket-id"
+  iam_arn = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity XXXXXXXXXXXXXXXXX"
+}
+```
---
+++ aws_s3_cdn_policy/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket_policy" "allow" {
+  bucket = var.bucket
+  policy = jsonencode({
+    "Version": "2008-10-17",
+    "Id": "PolicyForCloudFrontPrivateContent",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${var.iam_arn}"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket}/*"
+        }
+    ]
+  })
+}
