bf8511ab47114dc959c513e08147835aa5dcdb0c
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-12-25 17:18:11 +0200

    fixed variables

--- azure_rg/main.tf
+++ azure_rg/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "az_rg" {
-  name     = var.az_rg_name
-  location = var.az_rg_location
+  name     = var.rg_name
+  location = var.rg_location
 
   tags = var.rg_tags
 }
