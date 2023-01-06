bbb9a736ee48cbe2caa02003afc105664e54836d
Eugene Roslyakov <ei.roslyakov@gmail.com>
2023-01-06 15:13:04 +0200

    added id to output

--- aws_cdn_key_group/outputs.tf
+++ aws_cdn_key_group/outputs.tf
@@ -3,3 +3,8 @@ output "caller_reference" {
   value = aws_cloudfront_public_key.key.caller_reference
 }
 
+output "id" {
+  description = "The identifier for the key group."
+  value = aws_cloudfront_key_group.key_group.id
+}
+
