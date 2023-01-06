2c6777c75e6d16eb28dfe3b8e7bbab81f4390768
Eugene Roslyakov <ei.roslyakov@gmail.com>
2023-01-06 17:02:36 +0200

    some impr

--- aws_cdn_key_group/main.tf
+++ aws_cdn_key_group/main.tf
@@ -9,3 +9,15 @@ resource "aws_cloudfront_key_group" "key_group" {
   items   = [aws_cloudfront_public_key.key.id]
   name    = var.key_group_name
 }
+
+resource "aws_secretsmanager_secret" "secret" {
+  count = var.create_pub_key_secret ? 1 : 0
+  name = var.secret_name
+}
+ 
+ 
+resource "aws_secretsmanager_secret_version" "secret_v" {
+  secret_id = aws_secretsmanager_secret.secret[0].id
+  secret_string = try(var.secret_string, null)
+  secret_binary = try(var.secret_binary, null)
+}
