4e28fd97a93db76f4a6caf268771c06a71ac98e6
Yevhenii Rosliakov <ei.roslyakov@gmail.com>
2023-01-24 16:45:09 +0200

    Update version.tf

--- aws_ec2/version.tf
+++ aws_ec2/version.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3"
+      version = "~> 4"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.2.7"
 }
