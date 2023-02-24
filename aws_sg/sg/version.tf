b331a06aa0cef457de0be7fbe4f5eca6401f46f8
Yevhenii Rosliakov <ei.roslyakov@gmail.com>
2023-02-24 18:25:35 +0200

    Update version.tf

--- aws_sg/sg/version.tf
+++ aws_sg/sg/version.tf
@@ -1,9 +1,19 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3"
+#     }
+#   }
+#   required_version = ">= 1.2.7"
+# }
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3"
+      version = ">= 4.6"
     }
   }
-  required_version = ">= 1.2.7"
 }
